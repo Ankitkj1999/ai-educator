f5ef7fce26bde43be185245da68afe66e4fd4f99
Abhishek <abhisheksharma7124@gmail.com>
2025-05-18 15:49:46 +0530

    fix: Added lazyLoading on routes

--- src/App.js
+++ src/App.js
@@ -1,49 +1,55 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Landing from './pages/landing';
-import SignIn from './pages/signin';
-import SignUp from './pages/signup';
+import React, { lazy, Suspense } from 'react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import ForgotPassword from './pages/forgotpassword';
-import ResetPassword from './pages/resetpassword';
-import Home from './pages/home';
-import Create from './pages/create';
-import Topics from './pages/topics';
-import Course from './pages/course';
-import Certificate from './pages/certificate';
-import Profile from './pages/profile';
-import Pricing from './pages/pricing';
-import Features from './pages/features';
-import About from './pages/about';
-import Payment from './pages/payment';
-import Success from './pages/success';
-import Manage from './pages/manage';
-import Successful from './pages/successful';
-import Failed from './pages/failed';
-import Contact from './pages/contact';
-import DashBoard from './admin/dashboard';
-import Users from './admin/users';
-import Courses from './admin/courses';
-import PaidUsers from './admin/paidusers';
-import Admins from './admin/admins';
-import Contacts from './admin/contacts';
-import Terms from './admin/terms';
-import Privacy from './admin/privacy';
-import Cancellation from './admin/cancellation';
-import Refund from './admin/refund';
-import Billing from './admin/billing';
-import TermsPolicy from './pages/termspolicy';
-import PrivacyPolicy from './pages/privacypolicy';
-import CancelPolicy from './pages/cancelpolicy';
-import RefundPolicy from './pages/refundpolicy';
-import BillingPolicy from './pages/billingpolicy';
-import Error from './pages/error';
-import Pending from './pages/pending';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import { googleClientId } from './constants';
-import Shareable from './pages/shareable';
-import Exam from './pages/exam';
+import Loading from './pages/Loading';
+
+// Public Pages
+const Landing = lazy(() => import('./pages/landing'));
+const SignIn = lazy(() => import('./pages/signin'));
+const SignUp = lazy(() => import('./pages/signup'));
+const ForgotPassword = lazy(() => import('./pages/forgotpassword'));
+const ResetPassword = lazy(() => import('./pages/resetpassword'));
+const Home = lazy(() => import('./pages/home'));
+const Create = lazy(() => import('./pages/create'));
+const Topics = lazy(() => import('./pages/topics'));
+const Course = lazy(() => import('./pages/course'));
+const Certificate = lazy(() => import('./pages/certificate'));
+const Profile = lazy(() => import('./pages/profile'));
+const Pricing = lazy(() => import('./pages/pricing'));
+const Features = lazy(() => import('./pages/features'));
+const About = lazy(() => import('./pages/about'));
+const Payment = lazy(() => import('./pages/payment'));
+const Success = lazy(() => import('./pages/success'));
+const Manage = lazy(() => import('./pages/manage'));
+const Successful = lazy(() => import('./pages/successful'));
+const Failed = lazy(() => import('./pages/failed'));
+const Contact = lazy(() => import('./pages/contact'));
+const Shareable = lazy(() => import('./pages/shareable'));
+const Exam = lazy(() => import('./pages/exam'));
+const TermsPolicy = lazy(() => import('./pages/termspolicy'));
+const PrivacyPolicy = lazy(() => import('./pages/privacypolicy'));
+const CancelPolicy = lazy(() => import('./pages/cancelpolicy'));
+const RefundPolicy = lazy(() => import('./pages/refundpolicy'));
+const BillingPolicy = lazy(() => import('./pages/billingpolicy'));
+const Pending = lazy(() => import('./pages/pending'));
+const Error = lazy(() => import('./pages/error'));
+
+// Admin Pages
+const DashBoard = lazy(() => import('./admin/dashboard'));
+const Users = lazy(() => import('./admin/users'));
+const Courses = lazy(() => import('./admin/courses'));
+const PaidUsers = lazy(() => import('./admin/paidusers'));
+const Admins = lazy(() => import('./admin/admins'));
+const Contacts = lazy(() => import('./admin/contacts'));
+const Terms = lazy(() => import('./admin/terms'));
+const Privacy = lazy(() => import('./admin/privacy'));
+const Cancellation = lazy(() => import('./admin/cancellation'));
+const Refund = lazy(() => import('./admin/refund'));
+const Billing = lazy(() => import('./admin/billing'));
 
 function App() {
 
@@ -66,50 +72,52 @@ function App() {
           pauseOnHover
           theme={sessionStorage.getItem('darkMode') === 'true' ? 'dark' : 'light'}
         />
-        <Routes>
-          {/* Main App */}
-          <Route path="/" exact element={<Landing />} />
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path='/forgot' element={<ForgotPassword />} />
-          <Route path='/reset-password/:token' element={<ResetPassword />} />
-          <Route path='/home' element={<Home />} />
-          <Route path='/create' element={<Create />} />
-          <Route path='/topics' element={<Topics />} />
-          <Route path='/course' element={<Course />} />
-          <Route path='/certificate' element={<Certificate />} />
-          <Route path='/profile' element={<Profile />} />
-          <Route path='/pricing' element={<Pricing />} />
-          <Route path='/features' element={<Features />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/payment' element={<Payment />} />
-          <Route path='/success' element={<Success />} />
-          <Route path='/subscription' element={<Manage />} />
-          <Route path='/successful' element={<Successful />} />
-          <Route path='/failed' element={<Failed />} />
-          <Route path='/contact' element={<Contact />} />
-          <Route path='/terms' element={<TermsPolicy />} />
-          <Route path='/privacy' element={<PrivacyPolicy />} />
-          <Route path='/cancellation' element={<CancelPolicy />} />
-          <Route path='/refund' element={<RefundPolicy />} />
-          <Route path='/billing' element={<BillingPolicy />} />
-          <Route path='/pending' element={<Pending />} />
-          <Route path='/shareable' element={<Shareable />} />
-          <Route path='/exam' element={<Exam />} />
-          {/* Admin Panel */}
-          <Route path='/dashBoard' element={<DashBoard />} />
-          <Route path='/users' element={<Users />} />
-          <Route path='/courses' element={<Courses />} />
-          <Route path='/paid' element={<PaidUsers />} />
-          <Route path='/admins' element={<Admins />} />
-          <Route path='/contacts' element={<Contacts />} />
-          <Route path='/editterms' element={<Terms />} />
-          <Route path='/editprivacy' element={<Privacy />} />
-          <Route path='/editcancellation' element={<Cancellation />} />
-          <Route path='/editrefund' element={<Refund />} />
-          <Route path='/editbilling' element={<Billing />} />
-          <Route path="*" element={<Error />} />
-        </Routes>
+        <Suspense fallback={<Loading/>}>
+          <Routes>
+            {/* Main App */}
+            <Route path="/" exact element={<Landing />} />
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path='/forgot' element={<ForgotPassword />} />
+            <Route path='/reset-password/:token' element={<ResetPassword />} />
+            <Route path='/home' element={<Home />} />
+            <Route path='/create' element={<Create />} />
+            <Route path='/topics' element={<Topics />} />
+            <Route path='/course' element={<Course />} />
+            <Route path='/certificate' element={<Certificate />} />
+            <Route path='/profile' element={<Profile />} />
+            <Route path='/pricing' element={<Pricing />} />
+            <Route path='/features' element={<Features />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/payment' element={<Payment />} />
+            <Route path='/success' element={<Success />} />
+            <Route path='/subscription' element={<Manage />} />
+            <Route path='/successful' element={<Successful />} />
+            <Route path='/failed' element={<Failed />} />
+            <Route path='/contact' element={<Contact />} />
+            <Route path='/terms' element={<TermsPolicy />} />
+            <Route path='/privacy' element={<PrivacyPolicy />} />
+            <Route path='/cancellation' element={<CancelPolicy />} />
+            <Route path='/refund' element={<RefundPolicy />} />
+            <Route path='/billing' element={<BillingPolicy />} />
+            <Route path='/pending' element={<Pending />} />
+            <Route path='/shareable' element={<Shareable />} />
+            <Route path='/exam' element={<Exam />} />
+            {/* Admin Panel */}
+            <Route path='/dashBoard' element={<DashBoard />} />
+            <Route path='/users' element={<Users />} />
+            <Route path='/courses' element={<Courses />} />
+            <Route path='/paid' element={<PaidUsers />} />
+            <Route path='/admins' element={<Admins />} />
+            <Route path='/contacts' element={<Contacts />} />
+            <Route path='/editterms' element={<Terms />} />
+            <Route path='/editprivacy' element={<Privacy />} />
+            <Route path='/editcancellation' element={<Cancellation />} />
+            <Route path='/editrefund' element={<Refund />} />
+            <Route path='/editbilling' element={<Billing />} />
+            <Route path="*" element={<Error />} />
+          </Routes>
+        </Suspense>
       </div>
     </Router>
     </GoogleOAuthProvider>
